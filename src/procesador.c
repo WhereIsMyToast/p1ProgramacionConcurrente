6f858e14f7a80561574a42785929ee3032aec433
WhereIsMyToast <juanjo13204@hotmail.com>
2024-03-11 17:32:41 +0100

    proceso procesador

--- src/procesador.c
+++ src/procesador.c
@@ -1,8 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-  // TODO
+  char* nombre_fichero = argv[1];
+  char* patron = argv[2];
 
+  FILE* archivo = fopen(nombre_fichero, "r");
+  char linea[256]; 
+  int lineas = 0;
+  for(int lin = 1; fgets(linea, sizeof(linea), archivo);lin++){
+    lineas++;
+    linea[strcspn(linea, "\n")] = 0;
+    if (strstr(linea, patron) != NULL) {
+      printf("[PROCESADOR %d] Patrón '%s' encontrado en línea %d\n", getpid(), patron, lineas);
+    }
+  }
+  fclose(archivo);
   return EXIT_SUCCESS;
 }
